c7e5786d7f60df05b3b10b9285bbb791c6a1e40b
江心才子-攀升 <270628297@qq.com>
2024-11-02 01:11:28 +0800

    Add: ChatMessage

--- demo1/src/test/kotlin/com/example/studylangchain4j/StudyLangChain4JApplicationTest.kt
+++ demo1/src/test/kotlin/com/example/studylangchain4j/StudyLangChain4JApplicationTest.kt
@@ -1,5 +1,6 @@
 package com.example.studylangchain4j
 
+import dev.langchain4j.data.message.UserMessage
 import dev.langchain4j.model.chat.ChatLanguageModel
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
@@ -27,5 +28,18 @@ class StudyLangChain4JApplicationTest {
         println("chatLanguageModel=$chatLanguageModel")
         val generate = chatLanguageModel.generate("你好")
         println(generate)
+    }
+
+    @Test
+    fun testChatMessage() {
+        val generate = chatLanguageModel.generate(
+            listOf(
+                UserMessage.from("你好啊，这是谁"),
+                UserMessage.from("进军", "这是我的AI朋友"),
+                UserMessage.from("李白", "好的，问你个问题，咱们现在是几个人在聊天。都说了什么？"),
+                UserMessage.from("进军", "你知道我们的名字吗？"),
+            )
+        )
+        println(generate.content().text())
     }
 }
