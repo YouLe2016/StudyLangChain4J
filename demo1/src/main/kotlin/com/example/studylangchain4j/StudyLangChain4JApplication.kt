9957704722206edebc03850fdd66a177fb32b103
江心才子-攀升 <270628297@qq.com>
2024-11-01 22:32:56 +0800

    Initial commit

---
+++ demo1/src/main/kotlin/com/example/studylangchain4j/StudyLangChain4JApplication.kt
@@ -0,0 +1,11 @@
+package com.example.studylangchain4j
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyLangChain4JApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudyLangChain4JApplication>(*args)
+}
